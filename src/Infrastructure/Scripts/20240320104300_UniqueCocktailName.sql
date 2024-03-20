f66aacebf29b4bc060e0cbf400aabeb70bedc193
Sam G. <gaban.sam@gmail.com>
2024-03-20 11:44:01 +0100

    hot-add

---
+++ src/Infrastructure/Scripts/20240320104300_UniqueCocktailName.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+
+CREATE UNIQUE INDEX "IX_Cocktail_Name" ON "Cocktail" ("Name");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240320104300_UniqueCocktailName', '8.0.2');
+
+COMMIT;
+
